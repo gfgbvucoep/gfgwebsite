871c575172217cc3c26f0c5cd07bb447aaf02de8
sunildangi111 <149437045+sunildangi111@users.noreply.github.com>
2024-09-21 00:35:28 +0530

    form updated

--- scriptzz.js
+++ scriptzz.js
@@ -1,4 +1,25 @@
-document.getElementById('recruitment-form').addEventListener('submit', function(e) {
+// document.getElementById('recruitment-form').addEventListener('submit', function(e) {
+//     e.preventDefault();
+    
+//     const formData = new FormData(this);
+  
+//     fetch('https://script.google.com/macros/s/AKfycbwx1jWrjibQs4sNUh--O-ZhZezVhkOlDQjiaSWDez4SVac8Sl9UuZPvcfgHy3cyXk4U/exec', {  // Replace with your actual Web App URL
+//       method: 'POST',
+//       body: formData
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//       console.log('Success:', data);
+//       alert('Form submitted successfully!\n Please join Recruitment group for the further information \n https://chat.whatsapp.com/ImEzhVKZmjt9dijeY48LzV');
+//     })
+//     .catch((error) => {
+//       console.error('Error:', error);
+//       alert('Form submission failed! \n If you have an issue please contact \n Kshitij Tomar  9039269205 ');
+//     });
+//   });
+
+  // model Script
+  document.getElementById('recruitment-form').addEventListener('submit', function(e) {
     e.preventDefault();
     
     const formData = new FormData(this);
@@ -10,11 +31,19 @@ document.getElementById('recruitment-form').addEventListener('submit', function(
     .then(response => response.json())
     .then(data => {
       console.log('Success:', data);
-      alert('Form submitted successfully!');
+      // Show custom modal
+      document.getElementById('successModal').style.display = 'block';
     })
     .catch((error) => {
       console.error('Error:', error);
-      alert('Form submission failed!');
+      alert('Form submission failed! \n If you have an issue please contact \n 9039269205 ');
     });
   });
-  
+
+  // Close the modal when the user clicks on the close button
+  document.getElementById('closeModal').addEventListener('click', function() {
+    document.getElementById('successModal').style.display = 'none';
+  });
+
+
+
